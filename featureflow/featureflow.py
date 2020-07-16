93b8abbb2f1684532961404e6778ab370f9d187c
Shvein Anton <t0hashvein@gmail.com>
2020-07-16 14:27:45 +0500

    Featureflow(Client) added

--- featureflow/featureflow.py
+++ featureflow/featureflow.py
@@ -1,11 +1,27 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from polling_client import PollingClient
+from events import Events
+from evaluate import Evaluate
+
 
 class Featureflow:
+    DEFAULT_FEATURE_VARIANT = "off"
+
     def __init__(self, api_key, with_features=[]):
         """docstring for __init__"""
         self.api_key = api_key
         self._polloing_client = PollingClient(self)
-        self._events_client = EventsService(self)
+        self._events_client = Events(self)
 
+        self._features = {}
+
+        if with_features != []:
+            self._events_client.register_features(with_features)
+
+    def update_features(self, features):
+        self._features = features
+
+    def evaluate(feature_key, user=None):
+        return Evaluate(self._features.get(feature_key, {}), user)
